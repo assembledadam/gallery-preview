7dcf662ad95c83821b1a812e448afce66a26f4af
Adam McCann <adam@mccann.io>
2018-01-03 13:56:26 +0000

    Update subscript.sh

--- subscript.sh
+++ subscript.sh
@@ -2,11 +2,6 @@
 cd "$1"
 FILE=`ls | grep '.jpg' | sort -n | head -1`
 
-# realpath $1
-#
-# CWD=`pwd`
-# echo $PWD
-
 if [[ ! -z "${FILE// }" ]]; then
     OUTPUT=`echo "$1_$FILE" | sed 's/\///g' | sed 's/\ //g'`
     echo "Copying ${FILE} to ${3}/${OUTPUT:1}..."
